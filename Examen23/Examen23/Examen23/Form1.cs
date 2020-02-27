d1e2ae23374bfebb84312bd1180e9ff6e0851eeb
eduardo-amrmolejo <eduardomarmolejo2018@gmail.com>
2020-02-27 12:36:38 -0600

    Examen de la Unidad 2 y3
    Carpeta con contenido el examen de la unidad 2 y3

---
+++ Examen23/Examen23/Examen23/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen23
+{
+    public partial class frmTemperaturas : Form
+    {
+        ClassTemperatura objTemperaturas = new ClassTemperatura();
+        public frmTemperaturas()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            objTemperaturas.Temperatura();
+            objTemperaturas.temperaturas = cmbUniMedia.Text.ToString();
+            objTemperaturas.medida = int.Parse(txtGrados.Text.ToString());
+
+
+            lblCentígrados.Text = objTemperaturas.centi.ToString();
+            lblFahrenheit.Text = objTemperaturas.fahr.ToString();
+        }
+    }
+}
