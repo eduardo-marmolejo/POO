0d133e75297dc0091071b4eafd21e29ee0464b7c
eduardo-marmolejo <eduardomarmolejo2018@gmail.com>
2020-03-22 10:26:47 -0600

    Piedra, Papel y Tijeras

---
+++ UNIDAD 4/Piedra, Papel y Tijeras/PiedraPapelTijeras/ClassPPT.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiedraPapelTijeras
+{
+    class ClassPPT
+    {
+        private int posicionOriginalPlayer1, posicionOiginalPlayer2;
+        private int movimientosManos, tiempoSeleccion, tiempoTotalJuego;
+        private bool movimientoArriba; 
+        private String ganeActual;
+
+        public int PosicionOriginalPlayer1
+        {
+            set
+            {
+                posicionOriginalPlayer1 = value;
+            }
+            get
+            {
+                return posicionOriginalPlayer1;
+            }
+        }
+
+        public int PosicionOriginalPlayer2
+        {
+            set
+            {
+                posicionOiginalPlayer2 = value;
+            }
+            get
+            {
+                return posicionOiginalPlayer2;
+            }
+        }
+
+        public int MovimientosManos
+        {
+            set
+            {
+                movimientosManos = value;
+            }
+            get
+            {
+                return movimientosManos;
+            }
+        }
+
+        public int TiempoSeleccion
+        {
+            set
+            {
+                tiempoSeleccion = value;
+            }
+            get
+            {
+                return tiempoSeleccion;
+            }
+        }
+
+        public int TiempoTotalJuego
+        {
+            set
+            {
+                tiempoTotalJuego = value;
+            }
+            get
+            {
+                return tiempoTotalJuego;
+            }
+        }
+
+        public bool MovimientoArriba
+        {
+            set
+            {
+                movimientoArriba = value;
+            }
+            get
+            {
+                return movimientoArriba;
+            }
+        }
+
+        public String GaneActual
+        {
+            set
+            {
+                ganeActual = value;
+            }
+            get
+            {
+                return ganeActual;
+            }
+        }
+
+        public ClassPPT()
+        {
+            TiempoTotalJuego = 0;
+        }
+    }
+}
---
+++ UNIDAD 4/Piedra, Papel y Tijeras/PiedraPapelTijeras/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PiedraPapelTijeras
+{
+    public partial class Form1 : Form,Ippt
+    {
+        ClassPPT objPPT = new ClassPPT();
+        ClassIniciar objIniciar = new ClassIniciar();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            objPPT.PosicionOriginalPlayer1 = pictureBox1.Location.Y;
+            objPPT.PosicionOriginalPlayer2 = pictureBox2.Location.Y;
+            timer3.Enabled = true;
+            timer3.Start();
+            IniciarJuego();
+            Sonido("readygo");
+
+        }
+        public void IniciarJuego() {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            timer1.Start();
+            timer2.Start();
+            objIniciar.IniciarJuego();
+        }
+
+        public void Sonido(String NombreSonido) {
+            var sonido = (Stream)Properties.Resources.ResourceManager.GetObject(NombreSonido);
+            SoundPlayer SonidoCargado = new SoundPlayer(sonido);
+            SonidoCargado.Play();
+
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int posicion = pictureBox1.Location.Y;
+            int velocidadMovimiento = 30;
+
+            if ((posicion < 281) && (objIniciar.MovimientoArriba == false))
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + velocidadMovimiento);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y - velocidadMovimiento);
+                if (posicion == 263) { objIniciar.MovimientoArriba = true; }
+
+            }
+            if ((posicion > 59) && (objIniciar.MovimientoArriba == true))
+            {
+                pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y-velocidadMovimiento);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, pictureBox2.Location.Y+velocidadMovimiento);
+                if (posicion == 83) { objIniciar.MovimientoArriba = false; objIniciar.MovimientosManos++;}
+
+            }
+            if(objIniciar.MovimientosManos == 2){
+                timer1.Stop();
+                pictureBox1.Location = new Point(pictureBox1.Location.X, objPPT.PosicionOriginalPlayer1);
+                pictureBox2.Location = new Point(pictureBox2.Location.X, objPPT.PosicionOriginalPlayer2);
+
+                Random rnd = new Random();
+                int player1 = rnd.Next(1, 4);
+                int player2 = rnd.Next(1, 4);
+
+                pictureBox1.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject(player1.ToString());
+                pictureBox2.Image = (Bitmap)Properties.Resources.ResourceManager.GetObject(player2 + "_d");
+                timer2.Start();
+                rojo.Enabled = true; 
+                azul.Enabled = true; 
+                empate.Enabled = true;
+
+                //1.- Tijera
+                //2.- Piedra
+                //3.- Papel
+
+                if (((player1 == 1) && (player2 == 2)) || ((player1 == 2) && (player2 == 3)) || ((player1 == 3) && (player2 == 1)))
+                {
+                    objIniciar.GaneActual = "azul";
+                }
+                if (((player1 == 1) && (player2 == 3)) || ((player1 == 2) && (player2 == 1)) || ((player1 == 3) && (player2 == 2)))
+                {
+                    objIniciar.GaneActual = "rojo";
+                }
+
+
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            objIniciar.TiempoSeleccion--;
+            lblTiempo.Text = objIniciar.TiempoSeleccion.ToString() + " ms";
+            if (objIniciar.TiempoSeleccion <= 0){
+                timer2.Stop();
+                IniciarJuego();
+                Sonido("timeover");
+            }
+
+        }
+
+        private void Verificar(object sender, EventArgs e)
+        {
+            String Presionado = ((PictureBox)sender).Name;
+            if ((Presionado == objIniciar.GaneActual) && (objIniciar.TiempoSeleccion > 0)) {
+                lblPuntos.Text = (Convert.ToInt32(lblPuntos.Text) + 1).ToString();
+                timer2.Stop();
+                IniciarJuego();
+                Sonido("Punto");
+            }
+            else {
+
+                timer2.Stop();
+                IniciarJuego();
+                Sonido("error");
+            }
+            rojo.Enabled = false; azul.Enabled = false; empate.Enabled = false;
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (objPPT.TiempoTotalJuego == (1000 * 1)) {
+
+                timer1.Stop();
+                timer2.Stop();
+                timer3.Stop();
+                Sonido("gameover");
+                DialogResult result = MessageBox.Show("Tiempo agotado - Puntaje Final:" + lblPuntos.Text, "Tiempo agotado", MessageBoxButtons.OK);
+                if (result == DialogResult.OK) {
+
+                    lblPuntos.Text = "0";
+                    timer3.Enabled = true;
+                    timer3.Start();
+                    IniciarJuego();
+                    Sonido("readygo");
+                
+                }
+            }
+            objPPT.TiempoTotalJuego++;
+        }
+    }
+}
