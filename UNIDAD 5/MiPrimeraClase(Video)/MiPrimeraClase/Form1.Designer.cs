02cedc9da94394cbc72e01eb4940c6ffd06638e8
eduardo-marmolejo <eduardomarmolejo2018@gmail.com>
2020-04-03 20:16:34 -0600

    MiPrimeraClase(Video)

---
+++ UNIDAD 5/MiPrimeraClase(Video)/MiPrimeraClase/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace MiPrimeraClase
+{
+    partial class frmPersonas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPersonas));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsbNuevo = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsbNuevo});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(535, 39);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsbNuevo
+            // 
+            this.tsbNuevo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.tsbNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tsbNuevo.Image")));
+            this.tsbNuevo.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.tsbNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbNuevo.Name = "tsbNuevo";
+            this.tsbNuevo.Size = new System.Drawing.Size(36, 36);
+            this.tsbNuevo.Text = "toolStripButton1";
+            this.tsbNuevo.ToolTipText = "Permite ingresar una nueva persona";
+            this.tsbNuevo.Click += new System.EventHandler(this.tsbNuevo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(66, 52);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 20);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNombre.Location = new System.Drawing.Point(66, 75);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(439, 20);
+            this.txtNombre.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtApellidos.Location = new System.Drawing.Point(66, 101);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(439, 20);
+            this.txtApellidos.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Apellidos:";
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCorreo.Location = new System.Drawing.Point(66, 127);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(439, 20);
+            this.txtCorreo.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "E-Mail:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Fecha de Nacimiento:";
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(130, 154);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(200, 20);
+            this.dtpFechaNacimiento.TabIndex = 10;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSalario.Location = new System.Drawing.Point(405, 153);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(100, 20);
+            this.txtSalario.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(352, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Salario:";
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(16, 189);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(489, 150);
+            this.dgvDatos.TabIndex = 13;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // frmPersonas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 354);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmPersonas";
+            this.Text = "Personas";
+            this.Load += new System.EventHandler(this.frmPersonas_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tsbNuevo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ UNIDAD 5/MiPrimeraClase(Video)/MiPrimeraClase/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimeraClase
+{
+    public partial class frmPersonas : Form
+    {
+        ArrayList Personas = new ArrayList();
+
+        public frmPersonas()
+        {
+            InitializeComponent();
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmPersonas_Load(object sender, EventArgs e)
+        {
+            ClassPersona Persona1 = new ClassPersona();
+            Persona1.ID = "1010";
+            Persona1.Nombre = "Lady";
+            Persona1.Apellidos = "Gaga";
+            Persona1.Correo = "ladygaga@gmail.com";
+            Persona1.FechaNacimiento = new DateTime(1990,3,14);
+            Persona1.Salario = 1200000;
+            Personas.Add(Persona1);
+
+            ClassPersona Persona2 = new ClassPersona();
+            Persona2.ID = "2020";
+            Persona2.Nombre = "Daddy";
+            Persona2.Apellidos = "Yankee";
+            Persona2.Correo = "daddy@yahoo.es";
+            Persona2.FechaNacimiento = new DateTime(1987,7,20);
+            Persona2.Salario = 2560000;
+            Personas.Add(Persona2);
+
+            dgvDatos.DataSource = Personas;
+        }
+
+        private void tsbNuevo_Click(object sender, EventArgs e)
+        {
+            if (txtID.Text == "")
+            {
+                errorProvider1.SetError(txtID, "Debe ingresar un ID de la persona");
+                txtID.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtID, "");
+
+            if (Existe(txtID.Text))
+            {
+                errorProvider1.SetError(txtID, "ID del empleado ya existe");
+                txtID.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtID, "");
+
+
+            if (txtNombre.Text == "")
+            {
+                errorProvider1.SetError(txtNombre, "Debe ingresar un Nombre de la persona");
+                txtNombre.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtNombre, "");
+
+            if (txtApellidos.Text == "")
+            {
+                errorProvider1.SetError(txtApellidos, "Debe ingresar un Apellido de la persona");
+                txtApellidos.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtApellidos, "");
+
+            Regex regEmail = new Regex(@"^(([^<>()[\]\\.,;:\s@\""]+"
+                                     + @"(\.[^<>()[\]\\.,;:\s@\""]+)*)|(\"".+\""))@"
+                                     + @"((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}]"
+                                     + @"\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+"
+                                     + @"[a-zA-Z]{2,}))$", 
+                                     RegexOptions.Compiled);
+
+            if (!regEmail.IsMatch(txtCorreo.Text))
+            {
+                errorProvider1.SetError(txtCorreo, "Debe ingresar una dirección de correo válida");
+                txtCorreo.Focus();
+                return;
+            }
+            errorProvider1.SetError(txtCorreo, "");
+
+            decimal Salario;
+            if (!Decimal.TryParse(txtSalario.Text, out Salario)) 
+            {
+                errorProvider1.SetError(txtSalario, "Debe ingresar números en el campo salario");
+                txtSalario.Focus();
+                return;
+            }
+
+            if (Salario<0)
+            {
+                errorProvider1.SetError(txtSalario, "Debe ingresar un número positivo");
+                txtSalario.Focus();
+                return;
+            }
+
+            ClassPersona miPersona = new ClassPersona();
+            miPersona.ID = txtID.Text;
+            miPersona.Nombre = txtNombre.Text;
+            miPersona.Apellidos = txtApellidos.Text;
+            miPersona.Correo = txtCorreo.Text;
+            miPersona.FechaNacimiento = dtpFechaNacimiento.Value;
+            miPersona.Salario =Salario;
+            Personas.Add(miPersona);
+
+            dgvDatos.DataSource = null;
+            dgvDatos.DataSource = Personas;
+
+            txtID.Clear();
+            txtNombre.Clear();
+            txtApellidos.Clear();
+            txtCorreo.Clear();
+            txtSalario.Clear();
+            txtID.Focus();
+        }
+
+        private bool Existe(string ID)
+        {
+            foreach (ClassPersona Persona in Personas)
+            {
+                if (Persona.ID == ID) return true;
+            }
+            return false;
+        }
+    }
+}
